4d233d5eb78b749c8ef5f28f6c5abfce2831fab4
curtain-r <zwq2428118613@163.com>
2020-11-10 08:16:57 +0800

    1

---
+++ Promise/Promise04.js
@@ -0,0 +1,127 @@
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+class Promise {
+    constructor(executor) {
+        this.status = PENDING;
+        this.onFulfilledCallback = [];
+        this.onRejectedCallback = [];
+
+        let resolve = value => {
+            if (this.status === PENDING) {
+                this.status = FULFILLED;
+                this.value = value;
+                this.onFulfilledCallback.forEach(callback => callback());
+            }
+        }
+        let reject = reason => {
+            if (this.status === PENDING) {
+                this.status = REJECTED;
+                this.reason = reason;
+                this.onRejectedCallback.forEach(callback => callback());
+            }
+        }
+
+        try {
+            executor(resolve, reject);
+        } catch (e) {
+            reject(e);
+        }
+    }
+
+    then (onFulfilled, onRejected) {
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason };
+
+        let nextPromise = new Promise((resolve, reject) => {
+            if (this.status === FULFILLED) {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(this.value);
+                        promiseResolutionProcedure(nextPromise, x, resolve, reject);
+                    } catch(e) {
+                        reject(e);
+                    }
+                })
+            } else if(this.status === REJECTED) {
+                setTimeout(() => {
+                    try {
+                        let x= onRejected(this.reason);
+                        promiseResolutionProcedure(nextPromise, x, resolve, reject);
+                    } catch(e) {
+                        reject(e);
+                    }
+                });
+            } else {
+                this.onFulfilledCallback.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onFulfilled(this.value);
+                            promiseResolutionProcedure(nextPromise, x, resolve, reject);
+                        } catch(e) {
+                            reject(e);
+                        }
+                    })
+                })
+                this.onRejectedCallback.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x= onRejected(this.reason);
+                            promiseResolutionProcedure(nextPromise, x, resolve, reject);
+                        } catch(e) {
+                            reject(e);
+                        }
+                    });
+                })
+            }
+        })
+        return nextPromise;
+    }
+}
+
+// 最难的promiseResolutionProcedure
+
+function promiseResolutionProcedure(promise, x, resolve, reject) {
+    if (x === promise) {
+        reject(new TypeError('chaining cycle'));
+    }
+    if (x && typeof x === 'object' || typeof x === 'function') {
+        let used = false;
+        try {
+            let then = x.then;
+            if (typeof then === 'function') {
+                then.call(x,y => {
+                    if (used) return;
+                    used = true;
+                    promiseResolutionProcedure(promise, y ,resolve, reject);
+                },reason => {
+                    if(used) return;
+                    used = true;
+                    reject(reason);
+                })
+            } else {
+                if (used) return;
+                used = true;
+                resolve(x);
+            }
+        } catch(e) {
+            if (used) return;
+            used = true;
+            reject(e);
+        }
+    } else {
+        resolve(x);
+    }
+}
+
+Promise.defer = Promise.deferred = function() {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+};
+
+module.exports = Promise;
